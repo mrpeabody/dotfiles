588bf2d5dd2456d22494eb346a52a06634b8ab72
mrpeabody <pasha.rgmu@gmail.com>
2025-06-14 21:08:25 -0500

    Fix fonts script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+
+# Base dir is where the script is located
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+
+# Determine the user fonts location, OS-dependent
 FONT_SRC="$BASE_DIR/fonts"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     FONT_DST="$HOME/Library/Fonts"
 else
     FONT_DST="$HOME/.local/share/fonts"
 fi
-
 mkdir -p "$FONT_DST"
 
-echo "Installing font directories from $FONT_SRC to $FONT_DST..."
 
+echo "Installing font directories from $FONT_SRC to $FONT_DST..."
 for dir in "$FONT_SRC"/*/; do
     font_name=$(basename "$dir")
     target_dir="$FONT_DST/$font_name"
@@ -25,10 +30,12 @@ for dir in "$FONT_SRC"/*/; do
     cp -r "$dir" "$FONT_DST/"
 done
 
+
 # Refresh font cache (Linux only)
 if [[ "$OSTYPE" != "darwin"* ]]; then
     echo "Updating font cache..."
     fc-cache -f "$FONT_DST"
 fi
 
-echo "Font installation complete."
+
+echo "Font installation complete"
